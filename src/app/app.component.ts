cc8ea661e953dae14954d137e9c372e338dd902b
Santiago Vallejos <vallejossantiago1412@gmail.com>
2024-09-21 12:17:26 -0300

    Diseno de To-do List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TaskComponent } from "./components/task/task.component";
+import { TaskListComponent } from "./components/task-list/task-list.component";
+import { AddTaskComponent } from "./components/add-task/add-task.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TaskComponent, TaskListComponent, AddTaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
